d559381a98f784027328ebee3bf37aed65672bdd
Ruben Moya <ruben.moya@factorial.co>
2026-06-30 18:53:52 +0200

    feat: add github reporter and coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,19 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   test: {
-    include: ["test/**/*.test.ts"],
+    include: ["src/**/*.test.ts"],
     environment: "node",
     passWithNoTests: true,
+    reporters:
+      process.env.GITHUB_ACTIONS === "true"
+        ? ["dot", ["github-actions", { displayAnnotations: false }]]
+        : ["default"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/*.test.ts", "src/index.ts"],
+      reporter: ["text"],
+    },
   },
   resolve: {
     alias: {
